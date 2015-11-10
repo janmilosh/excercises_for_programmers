ce2cecb806a04a8f9b81ec9b3bee0004669bed11
janmilosh <janmilosh@gmail.com>
2015-11-09 21:20:47 -0500

    Adds room area calculator

---
+++ ch3_calculations/area.rb
@@ -0,0 +1,48 @@
+ENGLISH_TO_METRIC = 0.09290304
+
+class RectangleArea
+  attr_accessor :length, :width
+
+  def prompt text
+    print text
+    gets.chomp
+  end
+
+  def get_valid_length
+    @length = (prompt "\nWhat is the length of the room in feet? ").to_i
+    if @length <= 0
+      puts 'Please input a positive integer value.'
+      get_valid_length
+    end
+  end
+
+  def get_valid_width
+    @width = (prompt "What is the width of the room in feet? ").to_i
+    if @width <= 0
+      puts 'Please input a positive integer value.'
+      get_valid_width
+    end
+  end
+
+  def calculate_english
+    @length * @width
+  end
+
+  def calculate_metric
+    (@length * @width * ENGLISH_TO_METRIC).round(3)
+  end
+
+  def output
+    puts "You entered dimensions of #{ @length } feet by #{ @width } feet."
+    puts "The area is\n#{ calculate_english } square feet\n#{ calculate_metric } square meters\n\n"
+  end
+
+  def run
+    get_valid_length
+    get_valid_width
+    output
+  end
+end
+
+area = RectangleArea.new
+area.run
---
+++ spec/area_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require_relative '../ch3_calculations/area'
+
+describe 'RectangleArea' do 
+  let(:area) { RectangleArea.new }
+
+  before do
+    area.length = 15
+    area.width = 20
+  end
+
+  it 'should calculate area in English units' do
+    expect(area.calculate_english).to eq 300
+  end
+
+  it 'should calculate area in Metric units' do
+    expect(area.calculate_metric).to eq 27.871
+  end
+end
