552be5a827b374d2d59621a080398642a88a2d2c
janmilosh <janmilosh@gmail.com>
2015-12-06 20:23:33 -0500

    Adds number guessing game

---
+++ ch6_repetition/guess_the_numbers.rb
@@ -0,0 +1,121 @@
+require 'pry'
+
+class NumberGuesser
+  attr_reader :message, :level_data
+  attr_accessor :level, :number, :guesses
+
+  def initialize
+    @another_game = true
+    @play_game = true
+    @guesses = 0
+    @message = "Let's play Guess the Number."
+    @level = '1'
+    @level_data = { 
+        '1' => { 'min' => 1, 'max' =>   10},
+        '2' => { 'min' => 1, 'max' =>  100},
+        '3' => { 'min' => 1, 'max' => 1000}
+      }
+  end
+
+  def set_number_to_guess
+    num = Random.new
+    @number = num.rand(min..max)
+  end
+
+  def max
+    @level_data[@level]['max']
+  end
+
+  def min
+    @level_data[@level]['min']
+  end
+
+  def guess(input)
+    @guesses += 1
+    evaluate(input.to_i) if valid_number?(input)
+  end
+
+  def valid_level?
+    if !level_data.keys.include? @level
+      @message = 'Please choose 1, 2, or 3: '
+      return false
+    end
+    true
+  end
+
+  def valid_number?(input)
+    if !input.match /^\d+$/
+      @message = 'Not an integer, guess again: '
+      return false
+    end
+    true
+  end
+
+  def comment
+    case @guesses
+    when 1
+      return "You're a mind reader!"
+    when 2, 3, 4
+      return "Most impressive."
+    when 5, 6
+      return "You can do better than that."
+    else
+      return "Better luck next time."
+    end
+  end
+  
+  def evaluate(input)
+    if input > @number
+      @message = 'Too high. Guess again: '
+    elsif input < @number
+      @message = 'Too low. Guess again: '
+    elsif input == @number
+      @message = "You got it in #{ @guesses } guesses!\n#{ comment }"
+      @play_game = false
+    end
+  end
+
+  def get_level
+    print @message
+    @level = gets.chomp
+    get_level if !valid_level?
+  end
+
+  def start_game
+    @message = 'Pick a difficulty level (1, 2, or 3): '
+    get_level
+    set_number_to_guess
+    @message = "I have my number. What's your guess (from #{ min } to #{ max })? "
+  end
+
+  def set_another_game
+    input = gets.chomp.downcase[0]
+    if input == 'y'
+      initialize
+      @message = "Ok, let's play again then!"
+    elsif input == 'n'
+      @another_game = false
+      puts 'Goodbye!'
+    else
+      print 'Please input Yes or No: '
+      set_another_game
+    end
+  end
+
+  def run
+    while @another_game
+      puts @message
+      start_game    
+      while @play_game
+        print @message
+        guess(gets.chomp)
+      end
+      puts @message
+      print 'Play again? '
+      set_another_game
+    end
+  end
+end
+
+# game = NumberGuesser.new
+# game.run
---
+++ spec/guess_the_numbers_spec.rb
@@ -0,0 +1,129 @@
+require 'spec_helper'
+require_relative '../ch6_repetition/guess_the_numbers'
+
+describe 'number guesser' do
+  let(:game) { NumberGuesser.new }
+  
+  it 'should have the right level choices' do
+    expect(game.level_data.keys.sort).to eq ['1', '2', '3']
+  end
+
+  it 'should have the correct level 1 max and min' do
+    game.level = '1'
+    expect(game.min).to eq 1
+    expect(game.max).to eq 10
+  end
+
+  it 'should have the correct level 2 max and min' do
+    game.level = '2'
+    expect(game.min).to eq 1
+    expect(game.max).to eq 100
+  end
+
+  it 'should have the correct level 3 max and min' do
+    game.level = '3'
+    expect(game.min).to eq 1
+    expect(game.max).to eq 1000
+  end
+
+  it 'should validate a non-numeric guess' do
+    result = game.valid_number?('1abc')
+    expect(game.message).to eq "Not an integer, guess again: "
+  end
+
+  it 'should validate a non-integer guess' do
+    result = game.valid_number?('1.1')
+    expect(game.message).to eq "Not an integer, guess again: "
+  end
+
+  it 'should count a non-numeric guess as a guess' do
+    expect(game.guesses).to eq 0
+    game.guess('abc')
+    expect(game.guesses).to eq 1 
+  end
+
+  it 'should validate a wrong numeric level entry' do
+    game.level = '5'
+    result = game.valid_level?
+    expect(result).to eq false
+    expect(game.message).to eq 'Please choose 1, 2, or 3: '
+  end
+
+  it 'should validate a wrong non-numeric level entry' do
+    game.level = '1khf'
+    result = game.valid_level?
+    expect(result).to eq false
+    expect(game.message).to eq 'Please choose 1, 2, or 3: '
+  end
+
+  it 'should have an ititial level' do
+    expect(game.level).to eq '1'
+  end
+
+  it 'should set the level' do
+    game.level = '2'
+    expect(game.level).to eq '2'
+  end
+
+  it 'should set a level 1 number' do
+    game.level = '1'
+    result = game.set_number_to_guess
+    expect(result).to be >= game.min
+    expect(result).to be <= game.max
+  end
+
+  it 'should set a level 2 number' do
+    game.level = '2'
+    result = game.set_number_to_guess
+    expect(result).to be >= game.min
+    expect(result).to be <= game.max
+  end
+
+  it 'should set a level 3 number' do
+    game.level = '3'
+    result = game.set_number_to_guess
+    expect(result).to be >= game.min
+    expect(result).to be <= game.max
+  end
+
+  it 'should evaluate a valid high guess' do
+    game.number = 3
+    result = game.evaluate(6)
+    expect(game.message).to eq 'Too high. Guess again: '
+  end
+
+  it 'should evaluate a valid low guess' do
+    game.number = 6
+    result = game.evaluate(3)
+    expect(game.message).to eq 'Too low. Guess again: '
+  end
+
+  it 'should evaluate a correct guess' do
+    game.number = 20
+    game.guess('5')
+    game.guess('8')
+    game.guess('20')
+    expect(game.message).to eq 'You got it in 3 guesses!
+Most impressive.'
+  end
+
+  it 'should return the correct comment for 1 guess' do
+    game.guesses = 1
+    expect(game.comment).to eq "You're a mind reader!"
+  end
+
+  it 'should return the correct comment for 3 guesses' do
+    game.guesses = 3
+    expect(game.comment).to eq "Most impressive."
+  end
+
+  it 'should return the correct comment for 5 guesses' do
+    game.guesses = 5
+    expect(game.comment).to eq "You can do better than that."
+  end
+
+  it 'should return the correct comment for 7 guesses' do
+    game.guesses = 7
+    expect(game.comment).to eq "Better luck next time."
+  end
+end
